60ec27567de3dc403a5245b751a66c7cbfb39273
ZABarton <zach.barton@gmail.com>
2016-06-04 16:21:38 -0500

    Add solution for problem 010

---
+++ 010/010.rb
@@ -0,0 +1,11 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+require 'prime'
+sum = 0
+Prime.each(2000000) do |x|
+	sum += x
+end
+
+p sum
